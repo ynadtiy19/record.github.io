5fedd4535e9374caf13f75965e091780469bd388
maludeuu <162901184+madurom@users.noreply.github.com>
2024-06-04 20:49:45 +0800

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Toaster } from "react-hot-toast"
+import ScreenRecorder from "./ScreenRecorder"
+
+function App() {
+
+  return (
+    <>
+      <ScreenRecorder />
+      <Toaster />
+    </>
+  )
+}
+
+export default App
